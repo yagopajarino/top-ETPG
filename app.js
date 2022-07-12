6928b1bd7d5549eaf65f419df508a8d2358f1f36
Yago Pajariño <yagopajarino@outlook.com>
2022-07-12 17:34:27 -0300

    Add app.js and bin/

---
+++ app.js
@@ -0,0 +1,52 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+require("dotenv").config();
+
+var indexRouter = require("./routes/index_routes");
+var politicosRouter = require("./routes/politicos_routes");
+var cargosRouter = require("./routes/cargos_routes");
+var gabinetesRouter = require("./routes/gabinetes_routes");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+let mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
+
+app.use("/", indexRouter);
+app.use("/politicos", politicosRouter);
+app.use("/cargos", cargosRouter);
+app.use("/gabinetes", gabinetesRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
