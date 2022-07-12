b1566680fe70dc04566ded771b6f39234921b68b
Yago Pajariño <yagopajarino@outlook.com>
2022-07-12 20:42:11 -0300

    Add nuevo cargo functionality

--- controllers/cargos_controller.js
+++ controllers/cargos_controller.js
@@ -16,9 +16,41 @@ exports.cargos_get = function (req, res, next) {
 };
 
 exports.nuevo_get = function (req, res, next) {
-  res.send("Pendiente nuevo cargo GET");
+  res.render("cargo_form", { title: "Nuevo cargo" });
 };
 
-exports.nuevo_post = function (req, res, next) {
-  res.send("Pendiente nuevo cargo POST");
-};
+exports.nuevo_post = [
+  body("nombre", "Completar nombre del cargo")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  function (req, res, next) {
+    let rank = req.body.nombre.match(".*Ministro.*") ? 3 : 4;
+    const errors = validationResult(req);
+    let cargo = new Cargo({ nombre: req.body.nombre, rank: rank });
+    if (!errors.isEmpty()) {
+      res.render("cargo_form", {
+        title: "Nuevo cargo",
+        cargo: cargo,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Cargo.findOne({ nombre: req.body.nombre }).exec(function (err, result) {
+        if (err) {
+          return next(err);
+        }
+        if (result) {
+          res.redirect("/cargos");
+        } else {
+          cargo.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            res.redirect("/cargos");
+          });
+        }
+      });
+    }
+  },
+];
