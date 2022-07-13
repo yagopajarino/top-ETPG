b443c7f115635da5e3e0a65e272345b85a0acbc8
Yago Pajariño <yagopajarino@outlook.com>
2022-07-13 19:37:27 -0300

    Add create gabinete post method

--- controllers/gabinetes_controller.js
+++ controllers/gabinetes_controller.js
@@ -2,7 +2,7 @@ const async = require("async");
 const { body, validationResult } = require("express-validator");
 const { render } = require("pug");
 const Gabinete = require("../models/gabinete");
-const nombramiento = require("../models/nombramiento");
+const Nombramiento = require("../models/nombramiento");
 const Politico = require("../models/politico");
 const Cargo = require("../models/cargo");
 
@@ -68,5 +68,53 @@ exports.nuevo_get = function (req, res, next) {
 };
 
 exports.nuevo_post = function (req, res, next) {
-  res.send("Pendiente nuevo gabinente POST");
+  async.parallel(
+    {
+      politicos: function (callback) {
+        Politico.find(callback);
+      },
+      cargos: function (callback) {
+        Cargo.find(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (Object.keys(req.body).length === 0) {
+        res.render("gabinete_form", {
+          title: "Nuevo gabinete",
+          politicos: results.politicos,
+          cargos: results.cargos,
+          errors: [{ msg: "Elegir al menos un político" }],
+        });
+        return;
+      }
+      let nombramientos = req.body.nombramiento;
+      let ns = [];
+      nombramientos.forEach((element) => {
+        let ids = element.split(";");
+        let p = results.politicos.find((a) => a._id == ids[1]);
+        let c = results.cargos.find((a) => a._id == ids[0]);
+        let n = new Nombramiento({ politico: p._id, cargo: c._id });
+        n.save(function (err) {
+          if (err) {
+            return next(err);
+          }
+        });
+        ns.push(n);
+      });
+      let gabinete = new Gabinete({
+        nombramientos: ns,
+        votos: 0,
+        creation: new Date(),
+      });
+      gabinete.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/gabinetes");
+      });
+    }
+  );
 };
